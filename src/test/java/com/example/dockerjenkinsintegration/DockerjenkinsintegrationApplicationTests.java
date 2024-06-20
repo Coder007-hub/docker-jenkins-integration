b811ad344548ce9099c88e039e474f4a01bf098f
sivaganesh8 <164253885+sivaganesh8@users.noreply.github.com>
2024-06-20 10:05:27 -0500

    first commit

---
+++ src/test/java/com/example/dockerjenkinsintegration/DockerjenkinsintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerjenkinsintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
